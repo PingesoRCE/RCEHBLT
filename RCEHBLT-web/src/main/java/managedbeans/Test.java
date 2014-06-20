a3424566c04feb0d3b557d801c22d9e41f7ca8a3
xarly1602 <>
2014-06-20 18:41:25 -0400

    Managed Bean vacio para dar jugo

---
+++ RCEHBLT-web/src/main/java/managedbeans/Test.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package managedbeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author DevelUser
+ */
+@ManagedBean
+@RequestScoped
+public class Test {
+
+    /**
+     * Creates a new instance of Test
+     */
+    public Test() {
+    }
+    
+}
